9542c04d6f9767885e0be69ada0bb4a025d53714
geoffroy <geoffroym2@gmail.com>
2019-03-21 13:28:20 +0100

    Intent creation Item

--- Todolist_Muller_Geoffroy_3/app/src/main/java/fr/iutnc/info/muller624u/todolist_muller_geoffroy_3/CreerItemActivity.java
+++ Todolist_Muller_Geoffroy_3/app/src/main/java/fr/iutnc/info/muller624u/todolist_muller_geoffroy_3/CreerItemActivity.java
@@ -3,9 +3,11 @@ package fr.iutnc.info.muller624u.todolist_muller_geoffroy_3;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class CreerItemActivity extends AppCompatActivity {
 
@@ -16,14 +18,22 @@ public class CreerItemActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        TextInputEditText textlab = findViewById(R.id.editTextLabel);
+        RadioButton rbfaible = findViewById(R.id.radioButtonFaible);
+        RadioButton rbnormal = findViewById(R.id.radiobuttonnormal);
+        RadioButton rbimportant = findViewById(R.id.radioButtonImportant);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with ", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+
+
     }
 
 }
